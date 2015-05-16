9ad26fa505b6760da73bf2e97427dcc2f5a770d1
Hiro51 <h.tsujino0714@gmail.com>
2015-05-16 09:45:02 +0900

    Initial Commit

---
+++ adWordsConversionTrackingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  adWordsConversionTrackingApp
+//
+//  Created by TsujinoHiroyuki on 5/16/15.
+//  Copyright (c) 2015 mict. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ adWordsConversionTrackingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  adWordsConversionTrackingApp
+//
+//  Created by TsujinoHiroyuki on 5/16/15.
+//  Copyright (c) 2015 mict. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
